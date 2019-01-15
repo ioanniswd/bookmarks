474635a4470a25d5d8fdc8689949f20263508f0c
ioanniswd <ioanniswd@gmail.com>
2019-01-15 12:56:08 +0200

    Added index and show for books. Showing reviews for book.
     On branch master
    
    	new file:   app/assets/javascripts/books.coffee
    	new file:   app/assets/stylesheets/books.scss
    	new file:   app/controllers/books_controller.rb
    	new file:   app/helpers/books_helper.rb
    	new file:   app/views/books/index.html.erb
    	new file:   app/views/books/show.html.erb
    	modified:   app/views/layouts/application.html.erb
    	modified:   config/routes.rb
    	new file:   test/controllers/books_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resource :books
+  root 'books#index'
+  get '/books/:id' => 'books#show', as: :book
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
